048f5dd45c11705b6c2fc44eba3367c44ab2ad7d
Jacob Beard <jbeard4@cs.mcgill.ca>
2012-08-05 21:26:01 -0400

    updated drawing tool.

--- node-repl/repl+http-debug-listener.js
+++ node-repl/repl+http-debug-listener.js
@@ -1,6 +1,9 @@
 var repl = require('repl'),
     xml2jsonml = require('xml2jsonml'),
-    scion = require('scion');
+    scion = require('scion'),
+    request = require('request');
+
+var atom3serverUrl = "http://localhost:12345";
 
 //1 - 2. get the xml file and convert it to jsonml
 xml2jsonml.parseFile(process.argv[2],function(err,scxmlJson){
@@ -18,11 +21,21 @@ xml2jsonml.parseFile(process.argv[2],function(err,scxmlJson){
     //5. Use the statechart object model to instantiate an instance of the statechart interpreter. Optionally, we can pass to the construct an object to be used as the context object (the 'this' object) in script evaluation. Lots of other parameters are available.
     var interpreter = new scion.scxml.NodeInterpreter(model);
 
-    var clientListener = new scion.HTTPClientListener({
-        host : "localhost",
-        port : 1337
+    interpreter.registerListener({
+        onEntry : function(id){
+            request.post({
+                url : atom3serverUrl,
+                body : "stateid=" + id + "&command=" + "enter"
+            },function(){})
+        },
+        onExit : function(id){
+            request.post({
+                url : atom3serverUrl,
+                body : "stateid=" + id + "&command=" + "exit"
+            },function(){})
+        },
+        onTransition : function(){}   
     });
-    interpreter.registerListener(clientListener);
 
     interpreter.start();
 
--- node-repl/repl.sh
+++ node-repl/repl.sh
@@ -1,1 +1,6 @@
-NODE_NO_READLINE=1 node repl.js $1
+dn=`dirname $0`
+abspath=`cd $dn; pwd`
+
+#NODE_NO_READLINE=1 node repl.js $1
+NODE_NO_READLINE=1 node $abspath/repl+http-debug-listener.js $1
+
